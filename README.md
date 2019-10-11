bca80922fcf4e36c3a87b8b75ae72d1857bbe29c
fredoleary <fred@essentel.com>
2019-10-10 18:34:04 -0700

    tewaks

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # html_benchmark
+
+How scripts are made
+how keys are made and copied to remote ssh-keygen/ ssh-copy-id pi@192.168.50.24
+Git clone
+create server
+create client
+build client and update server
--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import ReactSpeedometer from "react-d3-speedometer"
 import logo from '../weka_logo.png';
 import background_img from '../weka_hpc.jpg';
+import { redBright } from 'ansi-colors';
+
+const segmentColors=[
+    "#bf616a",
+    "#d08770",
+    "#ebcb8b",
+    "#a3be8c",
+    "#b48ead",
+  ]
 
 export class HomePage extends Component{
     constructor(props) {
@@ -21,20 +30,18 @@ export class HomePage extends Component{
                 <div className="content" style = {{ backgroundImage:`url(${background_img})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
                     <div style = {{backgroundColor: 'rgba(255, 255, 255, .7)', height:"100%", withd:"100%"}}>
                         <div style = {{position:'absolute', top:'50%', left:'50%', margin:0, transform: 'translate(-50%, -50%)', marginTop:"80px"}}>
-                            <ReactSpeedometer value={this.state.benchmark} height={500} width={500} /> 
+                            <ReactSpeedometer value={this.state.benchmark} height={500} width={500} startColor= "#500AD2" endColor= "#8C0A32" /> 
                         </div>
-                        <div>
+                        {/* <div>
                             <button onClick={this.runBenchmark} style = {{height:'50px', marginTop:"10px", fontSize:18}} >{this.getBenchMarkText()}</button>
-                        </div>
+                        </div> */}
                      </div>
                 </div>
-               {/* <div className="footer">
-                    <div style = {{position:'absolute', top:'50%', left:'50%', margin:0, transform: 'translate(-50%, -50%)'}}>
-                        <button onClick={this.runBenchmark} >
-                            Run benchmark
+               <div className="footer">
+                        <button onClick={this.runBenchmark} style ={{marginTop:"15px", marginBottom:"15px",fontSize:18 }}>
+                        {this.getBenchMarkText()}
                         </button>
-                    </div>
-                </div> */}
+                 </div>
             </div>
          )
     }
